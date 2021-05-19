009b48898813e45891b4d8d7835e30e4c9f4aa7c
khoolood <khoolood.loolood@gmail.com>
2021-05-19 14:33:06 +0200

    app set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const COOL_NAMES = ['khouloud', 'louli', 'loulou', 'doudi', 'dadou']
+
 function App() {
+
+  const [name, setName] = useState('')
+  const [isCool, setIsCool] = useState(false)
+
+  useEffect(()=>{setIsCool(COOL_NAMES.includes(name.toLocaleLowerCase()))} , [name])
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
   return (
     <div className="App">
-      
+     <h1>Who is cool?</h1>
+     <input value ={name} onChange={handleChange} />
+     { name &&<p> {name} is {!isCool && "not"} cool </p>} 
     </div>
   );
 }
